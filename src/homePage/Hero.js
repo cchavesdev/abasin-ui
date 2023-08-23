3bb2b3e5d6bb0f2aad39d24ec8f09d7713ecd112
carlosfabianchaves <cchaves@isthmusit.com>
2023-08-23 10:47:56 -0600

    homepage  video

--- src/homePage/Hero.js
+++ src/homePage/Hero.js
@@ -3,18 +3,23 @@ import React from 'react'
 export const Hero = () => {
   return (
     <div class="dynamicCodeInsert sortableModule hero-image no-edit-bar" data-name="hero_scheduler"
-    data-tag="hero_scheduler">
-  
-    <div class="homepage-hero">
+      data-tag="hero_scheduler">
+
+      <div class="homepage-hero">
 
         <div class="hero-img" id="homePageHero">
+          <div id="video-bg">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/G4az_7LFIIw?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div class="hero-content">
             <h6 class="subheading">ARAPAHOE BASIN</h6>
             <h1 class="homepage-hero-title">LEGENDARY FOR A REASON</h1>
             <h5 class="subheading">OPENING FOR WINTER OCTOBER 2023..</h5>
+          </div>
         </div>
         <div class="blurred-bar">
         </div>
+      </div>
     </div>
-</div>
   )
 }
